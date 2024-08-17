07e42bef230df7c02738a6cf9820afc0e2aed42a
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-08-17 15:15:33 +0300

    gilka2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 const Home = () => {
+  const [contryList, setContryList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const data = await getCountries();
+        setContryList(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {contryList.length > 0 && <CountryList contryList={contryList} />}
+        {isLoading && <Loader />}
+        {error && <Heading title="Something went wrong ..." bottom />}
       </Container>
     </Section>
   );
